a0169ef37183ffa64c2feecb143e3cd94208481f
grantbradshaw <grant.bradshaw@outlook.com>
2016-01-28 08:42:00 +0000

    Implementing button for rendering phrase

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,12 @@
 # Homepage (Root path)
+require_relative 'sentence'
+
 get '/' do
+  @sentence = session[:current_phrase]
   erb :index
+end
+
+post '/generate' do
+  session[:current_phrase] = Sentence.sentence_creator
+  redirect '/'
 end
--- app/sentence.rb
+++ app/sentence.rb
@@ -1,51 +1,50 @@
-require 'json'
+module Sentence
 
-file = File.read('words.json')
-@generator = JSON.parse(file)
+  file = File.read('app/words.json')
+  @generator = JSON.parse(file)
 
+  def self.sentence_creator
+    
+    sentence = ""
+    
+    @generator["structure"].sample.each do |word|
+      case word
+      when "noun"
+        to_add =  @generator["words"]["noun"].sample + " "
+      when "verb"
+        to_add =  @generator["words"]["verb"].sample + " "
+      when "adjective"
+        to_add = @generator["words"]["adjective"].sample + " "
+      when "adverb"
+        to_add = @generator["words"]["adverb"].sample + " "
+      when "player"
+        to_add = @generator["athletes"][random_team].sample + " "
+      when "team"
+        to_add = random_team + " "
+      when "cliche"
+        to_add = @generator["words"]["cliche"].sample + " "
+      else
+        sentence << word + " "
+        next
+      end
 
-def sentence_creator
-  
-  sentence = ""
-  
-  @generator["structure"].sample.each do |word|
-    case word
-    when "noun"
-      to_add =  @generator["words"]["noun"].sample + " "
-    when "verb"
-      to_add =  @generator["words"]["verb"].sample + " "
-    when "adjective"
-      to_add = @generator["words"]["adjective"].sample + " "
-    when "adverb"
-      to_add = @generator["words"]["adverb"].sample + " "
-    when "player"
-      to_add = @generator["athletes"][random_team].sample + " "
-    when "team"
-      to_add = random_team + " "
-    when "cliche"
-      to_add = @generator["words"]["cliche"].sample + " "
-    else
-      sentence << word + " "
-      next
+      if sentence.include? to_add
+        return sentence_creator
+      else
+        sentence << to_add
+      end
     end
 
-    if sentence.include? to_add
-      return sentence_creator
-    else
-      sentence << to_add
-    end
+    canadian_speak(sentence.strip())
   end
 
-  canadian_speak(sentence.strip())
-end
+  private
+    def self.random_team
+      @generator["athletes"].keys.sample
+    end
 
-def random_team
-  @generator["athletes"].keys.sample
+    def self.canadian_speak(sentence)
+      sentence << ", eh?" if rand(10) == 5
+      sentence
+    end
 end
-
-def canadian_speak(sentence)
-  sentence << ", eh?" if rand(10) == 5
-  sentence
-end
-
-puts sentence_creator
