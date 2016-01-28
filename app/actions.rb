84cc603c893648a8d7dc3d8255341bacebb12e05
grantbradshaw <grant.bradshaw@outlook.com>
2016-01-28 11:02:29 +0000

    Formatting

--- app/actions.rb
+++ app/actions.rb
@@ -66,5 +66,6 @@ end
 post '/make_public' do # oof, public is a reserved word in ruby - we should not use that as a column name (or concatenate a ? to the end at least)
   #@collection = Collection.find_by(user_id: params[:id])
   #@collection.public = !@collection.public
+  #@collection.save!
   redirect "users/#{session[:user]}/collection"
 end
