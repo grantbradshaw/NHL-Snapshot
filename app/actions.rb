2b5849de710c32133d4d9b4af8479bd920f8b69d
Grant <grant.bradshaw@outlook.com>
2016-01-30 14:38:43 -0800

    Refactoring routes for team snapshot

--- app/actions.rb
+++ app/actions.rb
@@ -5,6 +5,7 @@ require_relative 'auth_actions'
 require_relative 'saved_phrase_actions'
 require_relative 'collection_actions'
 require_relative 'user_actions'
+require_relative 'team_snapshot_actions'
 require_relative 'scrape'
 
 helpers do
@@ -34,23 +35,4 @@ get '/' do
   erb :index
 end
 
-post '/team_view' do
-  redirect '/team_view'
-end
 
-get '/team_view' do
-  if params[:team_choice] && params[:team_choice] != "Select Team..."
-    @team_rank = WebScrape.team_rank(params[:team_choice])
-    @top_player = WebScrape.top_scoring_player(params[:team_choice])
-    @top_goalie = WebScrape.top_goalie(params[:team_choice])
-    @next_game = WebScrape.next_game(params[:team_choice])
-    @last_game = WebScrape.last_game(params[:team_choice])
-    @team = params[:team_choice]
-  end
-  @teams = WebScrape.all_teams.sort
-  erb :'team_view/index'
-end
-
-post '/team_select' do
-
-end
