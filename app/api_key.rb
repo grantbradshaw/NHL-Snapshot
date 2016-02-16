24b69337e714a814f16240df6deee2d786cb8011
grantbradshaw <grant.bradshaw@outlook.com>
2016-02-16 20:43:59 +0000

    Preparing for heroku

---
+++ app/api_key.rb
@@ -0,0 +1,10 @@
+class API_Key
+  class << self
+    def key
+      "HR9zC0F4Okb7r2SHbJZZYtl15"
+    end
+    def secret
+      "eBMdVVr5KEkTRq6IS4HGT0I9h3lkC8o8rI5UMaRilHvsFB0TKr"
+    end
+  end
+end
